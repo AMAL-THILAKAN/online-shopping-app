443c37fbd588f0159cdeea2198e80d014123a16e
AMAL-THILAKAN <amalthilakan111@gmail.com>
2023-08-15 13:46:54 +0530

    cart activity ....

--- app/src/main/java/com/example/onlineshopping/MyAdapter.java
+++ app/src/main/java/com/example/onlineshopping/MyAdapter.java
@@ -4,14 +4,11 @@ import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,11 +16,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
-
+    
     Context context;
-    private List<Item> items;
-    private AdapterView.OnItemClickListener listener;;
-
+    List<Item> items;
 
     public MyAdapter(Context context, List<Item> items) {
         this.context = context;
@@ -31,10 +26,11 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
     }
 
 
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.item_view, parent, false));
+        return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.item_view,parent,false));
     }
 
     @Override
@@ -46,76 +42,45 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
         holder.imageView.setImageResource(items.get(position).getImage());
 
 
+
     }
 
     @Override
     public int getItemCount() {
-        return items.size();
+        return  items.size();
     }
 
-    public class Viewholder extends RecyclerView.ViewHolder {
+public class ViewHolder extends RecyclerView.ViewHolder{
 
-        TextView pdtName, pdtDesc, pdtPrice;
-        ImageView pdtImg, cartLogo;
+        TextView pdtName,pdtDesc,pdtPrice;
+        ImageView pdtImg;
 
-        public Viewholder(View itemView) {
+        ImageView cartLogo;
 
-            super(itemView);
+    public ViewHolder(View itemView) {
+        super(itemView);
+
+        pdtName = itemView.findViewById(R.id.pdtName);
+        pdtDesc= itemView.findViewById(R.id.pdtDesc);
+        pdtPrice = itemView.findViewById(R.id.pdtPrice);
+        pdtImg = itemView.findViewById(R.id.imageView);
+
+        cartLogo = itemView.findViewById(R.id.cartLogo);
+
+        cartLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//DOING THIS NOW
+                int position  = getAdapterPosition();
 
 
 
-            pdtName = itemView.findViewById(R.id.pdtName);
-            pdtDesc = itemView.findViewById(R.id.pdtDesc);
-            pdtPrice = itemView.findViewById(R.id.pdtPrice);
-            pdtImg = itemView.findViewById(R.id.imageView);
-            cartLogo = itemView.findViewById(R.id.cartLogo);
+                cartLogo.setImageResource(R.drawable.icon_selector);
 
-//            Log.i("ABCD", (String) pdtName.getText().toString());
+            }
+        });
 
-//            cartLogo.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//
-//                    Toast.makeText(context.getApplicationContext(), pdtName.getText().toString(), Toast.LENGTH_SHORT).show();
-//                    cartLogo.setBackgroundColor(Color.parseColor("#00FF00"));
-//
-//                }
-//            });
-
-        }
 
     }
-
-//    public class ViewHolder extends RecyclerView.ViewHolder {
-//
-//        TextView pdtName, pdtDesc, pdtPrice;
-//        ImageView pdtImg;
-//
-//        ImageView cartLogo;
-//
-//        public ViewHolder(View itemView) {
-//
-//            super(itemView);
-
-//            pdtName = itemView.findViewById(R.id.pdtName);
-//            pdtDesc = itemView.findViewById(R.id.pdtDesc);
-//            pdtPrice = itemView.findViewById(R.id.pdtPrice);
-//            pdtImg = itemView.findViewById(R.id.imageView);
-//            Toast.makeText(context.getApplicationContext(), "1 2 3...", Toast.LENGTH_SHORT).show();
-//
-//            cartLogo.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-////DOING THIS NOW
-//                    int position = getAdapterPosition();
-//
-//
-////                    cartLogo.setBackgroundColor(Color.parseColor("#00FF00"));
-//
-//                }
-//            });
-
-
-//        }
-//    }
+}
 }
--- app/src/main/java/com/example/onlineshopping/MyViewHolder.java
+++ app/src/main/java/com/example/onlineshopping/MyViewHolder.java
@@ -14,6 +14,9 @@ public class MyViewHolder extends RecyclerView.ViewHolder {
 
     ImageView imageView,cartLogo;
     TextView name,desc,price;
+
+    TextView cartName,cartDesc,cartPrice;
+    ImageView cartImage;
     public MyViewHolder(@NonNull View itemView) {
         super(itemView);
 
@@ -23,13 +26,21 @@ public class MyViewHolder extends RecyclerView.ViewHolder {
         price = itemView.findViewById(R.id.pdtPrice);
         cartLogo = itemView.findViewById(R.id.cartLogo);
 
+        cartImage = itemView.findViewById(R.id.cartImageView);
+        cartName = itemView.findViewById(R.id.cartPdtName);
+        cartDesc = itemView.findViewById(R.id.cartPdtDesc);
+        cartPrice = itemView.findViewById(R.id.cartPdtPrice);
+
+
         cartLogo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int position = getAdapterPosition();
+
                 // Handle button click for the specific item at 'position'
                 // For example, you can call a method in your activity/fragment
                 // to perform the desired action.
+
                 Toast.makeText(v.getContext(), "Cart Clicked!" + String.valueOf(position), Toast.LENGTH_SHORT).show();
 
             }
